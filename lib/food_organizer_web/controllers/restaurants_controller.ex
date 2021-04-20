2416bbcb5f8e72284d617b26ed87f46c7fc7b6ef
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-20 12:28:34 -0300

    refactoring controller method

--- lib/food_organizer_web/controllers/restaurants_controller.ex
+++ lib/food_organizer_web/controllers/restaurants_controller.ex
@@ -2,14 +2,13 @@ defmodule FoodOrganizerWeb.RestaurantsController do
   use FoodOrganizerWeb, :controller
 
   alias FoodOrganizer.Restaurant
-  alias FoodOrganizer.Restaurants.Create
 
   alias FoodOrganizerWeb.FallbackController
 
   action_fallback FallbackController
 
   def create(conn, params) do
-    with {:ok, %Restaurant{} = restaurant} <- Create.call(params) do
+    with {:ok, %Restaurant{} = restaurant} <- FoodOrganizer.create_restaurant(params) do
       conn
       |> put_status(:created)
       |> render("create.json", restaurant: restaurant)
