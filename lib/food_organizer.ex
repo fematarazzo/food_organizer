b1505aecfeb80cc65173e41a7951f73b6b91b6fe
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-21 11:13:13 -0300

    show page and functions

--- lib/food_organizer.ex
+++ lib/food_organizer.ex
@@ -1,8 +1,11 @@
 defmodule FoodOrganizer do
   alias FoodOrganizer.Restaurants.Create, as: RestaurantCreate
   alias FoodOrganizer.Supplies.Create, as: SupplyCreate
+  alias FoodOrganizer.Supplies.Get, as: SupplyGet
 
   defdelegate create_restaurant(params), to: RestaurantCreate, as: :call
 
   defdelegate create_supply(params), to: SupplyCreate, as: :call
+
+  defdelegate get_supply(params), to: SupplyGet, as: :call
 end
--- lib/food_organizer_web/controllers/supplies_controller.ex
+++ lib/food_organizer_web/controllers/supplies_controller.ex
@@ -14,4 +14,12 @@ defmodule FoodOrganizerWeb.SuppliesController do
       |> render("create.json", supply: supply)
     end
   end
+
+  def show(conn, %{"id" => uuid}) do
+    with {:ok, %Supply{} = supply} <- FoodOrganizer.get_supply(uuid) do
+      conn
+      |> put_status(:ok)
+      |> render("show.json", supply: supply)
+    end
+  end
 end
