d3b040c1f2ffa09c5d8c0e9588c47d36296349b6
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-21 10:39:39 -0300

    supply delegate function

--- lib/food_organizer.ex
+++ lib/food_organizer.ex
@@ -1,4 +1,8 @@
 defmodule FoodOrganizer do
-  alias FoodOrganizer.Restaurants.Create
-  defdelegate create_restaurant(params), to: Create, as: :call
+  alias FoodOrganizer.Restaurants.Create, as: RestaurantCreate
+  alias FoodOrganizer.Supplies.Create, as: SupplyCreate
+
+  defdelegate create_restaurant(params), to: RestaurantCreate, as: :call
+
+  defdelegate create_supply(params), to: SupplyCreate, as: :call
 end
