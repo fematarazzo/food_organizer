d1afd54c5274d1c55ad08358e4af3ccb816ea427
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-21 10:36:40 -0300

    supplies table and schema

--- lib/food_organizer/restaurant.ex
+++ lib/food_organizer/restaurant.ex
@@ -1,26 +1,30 @@
 defmodule FoodOrganizer.Restaurant do
-  use Ecto.Schema
-  import Ecto.Changeset
+use Ecto.Schema
+import Ecto.Changeset
 
-  @primary_key {:id, :binary_id, autogenerate: true}
+alias FoodOrganizer.Supply
 
-  @required_params [:email, :name]
+@primary_key {:id, :binary_id, autogenerate: true}
 
-  @derive {Jason.Encoder, only: @required_params ++ [:id]}
-  
-  schema "restaurants" do
-    field :email, :string
-    field :name, :string
+@required_params [:email, :name]
 
-    timestamps()
-  end
+@derive {Jason.Encoder, only: @required_params ++ [:id]}
 
-  def changeset(params) do
-    %__MODULE__{}
-    |> cast(params, @required_params)
-    |> validate_required(@required_params)
-    |> validate_length(:name, min: 2)
-    |> validate_format(:email, ~r/@/)
-    |> unique_constraint([:email])
-  end
+schema "restaurants" do
+  field :email, :string
+  field :name, :string
+
+  has_many :supplies, Supply
+
+  timestamps()
+end
+
+def changeset(params) do
+  %__MODULE__{}
+  |> cast(params, @required_params)
+  |> validate_required(@required_params)
+  |> validate_length(:name, min: 2)
+  |> validate_format(:email, ~r/@/)
+  |> unique_constraint([:email])
+end
 end
