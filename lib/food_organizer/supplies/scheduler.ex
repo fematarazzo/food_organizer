2c951955430a409d839dccaa98b8bc5706703edb
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-22 18:54:25 -0300

    scheduler added for message sending

--- lib/food_organizer/supplies/scheduler.ex
+++ lib/food_organizer/supplies/scheduler.ex
@@ -1,17 +1,30 @@
 defmodule FoodOrganizer.Supplies.Scheduler do
   use GenServer
 
+  alias FoodOrganizer.Supplies.ExpirationNotification
+
   def init(state \\ %{}) do
+    schedule_notification()
     {:ok, state}
   end
 
-  # async
-  def handle_cast({:put, key, value}, state) do
-    {:noreply, Map.put(state, key, value)}
+  def handle_info(:generate, state) do
+    ExpirationNotification.send()
+    schedule_notification()
+    {:noreply, state}
   end
 
-  # sync
-  def handle_call({:get, key}, _from, state) do
-    {:reply, Map.get(state, key), state}
+  defp schedule_notification do
+    Process.send_after(self(), :generate, 1000 * 10)
   end
+
+  # # async
+  # def handle_cast({:put, key, value}, state) do
+  #   {:noreply, Map.put(state, key, value)}
+  # end
+  #
+  # # sync
+  # def handle_call({:get, key}, _from, state) do
+  #   {:reply, Map.get(state, key), state}
+  # end
 end
