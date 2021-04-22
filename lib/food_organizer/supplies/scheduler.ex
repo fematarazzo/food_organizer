537fa47bb57c6a853c57fa124ac41c743b544719
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-22 18:58:25 -0300

    client x server implemented

--- lib/food_organizer/supplies/scheduler.ex
+++ lib/food_organizer/supplies/scheduler.ex
@@ -3,11 +3,19 @@ defmodule FoodOrganizer.Supplies.Scheduler do
 
   alias FoodOrganizer.Supplies.ExpirationNotification
 
+  # CLIENT
+  def start_link(_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  # SERVER
+  @impl true
   def init(state \\ %{}) do
     schedule_notification()
     {:ok, state}
   end
 
+  @impl true
   def handle_info(:generate, state) do
     ExpirationNotification.send()
     schedule_notification()
