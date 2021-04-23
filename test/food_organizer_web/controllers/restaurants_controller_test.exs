a05e76dfa72a3dddf197ca57acb9e3c0bef11e28
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-23 15:28:09 -0300

    test controller added

---
+++ test/food_organizer_web/controllers/restaurants_controller_test.exs
@@ -0,0 +1,37 @@
+defmodule FoodOrganizer.RestaurantsControllerTest do
+  use FoodOrganizerWeb.ConnCase
+
+  describe "create/2" do
+    test "when all params are valid, creates the user", %{conn: conn} do
+      params = %{name: "Restauranis", email: "restau@nis.com"}
+
+      response =
+        conn
+        |> post(Routes.restaurants_path(conn, :create, params))
+        |> json_response(:created)
+
+      assert %{
+                "message" => "Restaurant created!",
+                "restaurant" => %{
+                  "email" => "restau@nis.com",
+                  "id" => _id,
+                  "name" => "Restauranis"
+                }
+              } = response
+    end
+
+    test "when there are invalid params, return error", %{conn: conn} do
+      params = %{email: "restau@nis.com"}
+
+      expected_response = %{"message" => %{"name" => ["can't be blank"]}}
+
+      response =
+        conn
+        |> post(Routes.restaurants_path(conn, :create, params))
+        |> json_response(:bad_request)
+
+      assert response = expected_response
+    end
+
+  end
+end
